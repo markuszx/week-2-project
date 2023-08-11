f375852a22ff376cab2136820895e08b538d2b8d
mark garcia <markkusag@outlook.com>
2023-08-11 11:17:30 -0500

    02 done

--- problems/02-tripler-map.js
+++ problems/02-tripler-map.js
@@ -5,7 +5,7 @@ array.
 
 Solve this using Array's `map()` method.
 
-Examples: 
+Examples:
 
 console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
 console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
@@ -13,8 +13,19 @@ console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
 
 let tripler = function(nums) {
     // Your code here
-};
+  let arr = []
+    nums.map(function (el){
+      let total = el*3
 
+      //arr = el*3
+
+      arr.push(total)
+    })
+    return arr
+
+};
+console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
+console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
