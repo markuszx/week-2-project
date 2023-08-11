a65cab56794ea6bb3d5ab2b94de0e2492e847ce3
mark garcia <markkusag@outlook.com>
2023-08-11 11:06:08 -0500

    01 done!

--- problems/01-avg-val-forEach.js
+++ problems/01-avg-val-forEach.js
@@ -15,7 +15,21 @@ console.log(avgVal([])); // null
 
 let avgVal = function(arr) {
     // Your code here
+    let sum =0;
+
+    if (!arr.length){return null}
+    arr.forEach(function(el){
+
+      sum += el;
+
+    }); return sum / arr.length
+
 };
+
+
+console.log(avgVal([5, 10])); // 7.5
+console.log(avgVal([3, 7, 2, 1, 2])); // 3
+console.log(avgVal([])); // null
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
