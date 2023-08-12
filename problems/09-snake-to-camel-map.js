d75c2da109138b1a418e914597d81835103c5511
mark <markkusag@outlook.com>
2023-08-12 13:23:06 -0500

    initial

--- problems/09-snake-to-camel-map.js
+++ problems/09-snake-to-camel-map.js
@@ -16,13 +16,36 @@ console.log(snakeToCamel('APp_ACADEMY_iS_cOol')); // 'AppAcademyIsCool'
 */
 
 function snakeToCamel(str) {
-    // Your code here
+
+
+
+
+
+
+
+    // let finalWord = ""
+    // str.split("_").forEach(function (el) {
+    //     let firstLetters = el[0].toUpperCase()
+    //     let everyOtherLetter = ""
+
+    //     for (let i = 1; i < el.length; i++) {
+    //         everyOtherLetter += el[i]
+    //     }
+    //     finalWord += firstLetters + everyOtherLetter
+
+    // })
+    // return finalWord
+
 }
 
+console.log(snakeToCamel('snakes_go_hiss')); // 'SnakesGoHiss'
+console.log(snakeToCamel('say_hello_world')); // 'SayHelloWorld'
+console.log(snakeToCamel('app_academy_is_cool')); // 'AppAcademyIsCool'
+console.log(snakeToCamel('APp_ACADEMY_iS_cOol')); // 'AppAcademyIsCool'
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
     module.exports = snakeToCamel;
 } catch (e) {
     module.exports = null;
-}
+}
