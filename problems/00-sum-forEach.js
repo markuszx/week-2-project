96cb545a63eb5fc5796e78fc1ad85fc5e80a169a
Ruben Polanco <andr3z.93@gmail.com>
2023-08-14 16:17:50 -0700

    shortened answer for 00

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -12,19 +12,14 @@ console.log(sumForEach([])); // 0
 */
 
 let sumForEach = function(arr) {
-  // Your code here
-  //create sum veriable
-  //use forEach method on arr
-  //add those els from for each to sum
-  //return sum
   let sum =0;
-  arr.forEach(function (el){
-    sum += el
-
-  })
+  arr.forEach(el => sum += el)
   return sum
-
 };
+
+console.log(sumForEach([4, 3, -1, 10])); // 16
+console.log(sumForEach([6, 7, 2])); // 15
+console.log(sumForEach([])); // 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
